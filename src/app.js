dfb1444bceea3dcb3c63337685a0df52afe95edf
PeritxCreate <143961072+PeritxCreate@users.noreply.github.com>
2023-11-08 18:37:21 +0000

    correcta generación de la frase

--- src/app.js
+++ src/app.js
@@ -2,10 +2,33 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let random1 = Math.floor(Math.random() * 4);
+  let random2 = Math.floor(Math.random() * 4);
+  let random3 = Math.floor(Math.random() * 3);
+  let random4 = Math.floor(Math.random() * 5);
+  replace(random1, random2, random3, random4);
 };
+
+function replace(random1, random2, random3, random4) {
+  let text =
+    who[random1] +
+    " " +
+    action[random2] +
+    " " +
+    what[random3] +
+    " " +
+    when[random4];
+  document.getElementById("excuse").innerHTML = text;
+}
