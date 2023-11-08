ff7454f5ec56317e4cef71efcd43eefcb3d54a34
PeritxCreate <143961072+PeritxCreate@users.noreply.github.com>
2023-11-08 19:18:58 +0000

    con imagenes

--- src/app.js
+++ src/app.js
@@ -19,6 +19,7 @@ window.onload = function() {
   let random3 = Math.floor(Math.random() * 3);
   let random4 = Math.floor(Math.random() * 5);
   replace(random1, random2, random3, random4);
+  image(random1);
 };
 
 function replace(random1, random2, random3, random4) {
@@ -31,4 +32,20 @@ function replace(random1, random2, random3, random4) {
     " " +
     when[random4];
   document.getElementById("excuse").innerHTML = text;
+}
+
+function image(random1) {
+  if (random1 === 0) {
+    document.getElementById("imagen").src =
+      "https://m.media-amazon.com/images/I/51xsQq9kUQL._AC_UF1000,1000_QL80_.jpg";
+  } else if (random1 === 1) {
+    document.getElementById("imagen").src =
+      "https://i.pinimg.com/736x/50/bb/bb/50bbbb065f1abeb98a7fa948adb02bdc.jpg";
+  } else if (random1 === 2) {
+    document.getElementById("imagen").src =
+      "https://t3.ftcdn.net/jpg/05/77/79/12/360_F_577791293_i3HhJIR4GIQxzcD1JNNVY3qNLohk1xGy.jpg";
+  } else {
+    document.getElementById("imagen").src =
+      "https://i0.wp.com/literaryyard.com/wp-content/uploads/2017/11/smiling-face-funny-bird-picture.jpg";
+  }
 }
